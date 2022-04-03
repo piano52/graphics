ce6191b5a9337474ba02edf4d6e3148a41599dc8
piano52 <102783868+piano52@users.noreply.github.com>
2022-04-03 18:48:09 +0900

    Add files via upload

--- sketch_220403a.js
+++ sketch_220403a.js
@@ -32,6 +32,40 @@ function setup() {
     for (var y = 0; y < rows; y++) {
       terrain[x][y] = 0; //specify a default value for now
     
+ var cols, rows;
+var scl = 29;
+var w = 3000;
+var h = 2500;
+var flying = 0;
+var terrain = [];
+let Bslider;
+let Xslider;
+
+function setup() {
+  createCanvas(windowWidth,windowHeight, WEBGL);
+  colorMode(RGB);
+  
+  Bslider = createSlider(0, 255, 60, 10);
+  Bslider.position(10, 10);
+  Bslider.style('width', '80px');
+  Xslider = createSlider(-500, 500, 60, 3);
+  Xslider.position(10, 20);
+  Xslider.style('width', '80px');
+  Yslider = createSlider(-400, 200, 100, 2);
+  Yslider.position(10, 30);
+  Yslider.style('width', '80px');
+ 
+   
+  {
+    
+  
+  cols = w / scl;
+  rows = h / scl;
+  for (var x = 0; x < cols; x++) {
+    terrain[x] = [];
+    for (var y = 0; y < rows; y++) {
+      terrain[x][y] = 0; //specify a default value for now
+    
   }
   }
 }
@@ -59,8 +93,8 @@ function draw() {
     beginShape(TRIANGLE_STRIP);
     for (var x = 0; x < cols; x++) {
       let v=terrain[x][y];
-      v=map(v, -200, 70, 0, 255);
-      fill(30, 150-v, v-70);
+      v=map(v, -200, 70, 0, 270);
+      fill(50, 140-v, v-70,240);
       vertex(x * scl, y * scl, terrain[x][y]);
       vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
      
